920fd49f715be79d55b3cd2b99096c45594482e6
friendshipkim <kwj962004@gmail.com>
2022-05-31 21:32:29 -0400

    named entity extraction from the train set

--- scripts/cache_xsum_ner_train.sh
+++ scripts/cache_xsum_ner_train.sh
@@ -3,4 +3,4 @@ source /home/wk247/.bashrc
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 
 conda activate xsum_analysis
-python ~/workspace/xsum_analysis/cache_xsum_ner.py
+python ~/workspace/xsum_analysis/ptb/cache_xsum_ner_train.py
